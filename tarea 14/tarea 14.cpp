f07798ca364352c1be9ad2c94628cb3eeac9430f
Darthsoviet <31465914+Darthsoviet@users.noreply.github.com>
2017-10-29 19:49:41 -0500

    Add files via upload

---
+++ tarea 14/tarea 14.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#define MAX 5
+
+int main (int argc, char *argv[]){
+int edad=0;
+const float PI=3.1416;
+
+printf("cual es tu edad:");
+scanf("%d",&edad);
+;
+printf("tu edad es =%d \n",edad);
+printf("maximo=%d\n",MAX);
+printf("valor de PI=%F",PI);
+
+return 0;
+}
+
